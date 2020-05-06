e2a0376c3b95f8594ced621d2f8e88ad5a348b56
ErikaQuinteros23 <00117420@uca.edu.sv>
2020-05-06 13:03:50 -0600

    Segundo commit

---
+++ Laboratorio1/ejercicio2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int dolares;
+    float euros = 1.33;
+
+    float conversion;
+
+    cout<<"ingrese la canidad de dolares a convertir";
+    cin>>dolares;
+    conversion = dolares*euros;
+
+    cout<<conversion;
+
+
+    return 0;
+}
